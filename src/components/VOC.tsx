f851e1b620731a319cce377554059e669ed78cfb
ymc9 <104139426+ymc9@users.noreply.github.com>
2023-06-10 12:20:19 +0800

    update

--- src/components/VOC.tsx
+++ src/components/VOC.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick-theme.css';
 import 'slick-carousel/slick/slick.css';
-import './carousel.css';
 
 function Tweet({ url, image }: { url: string; image: string }) {
     return (
