aede82ea6b27e44c50d0cc491842d1118f014f00
ymc9 <104139426+ymc9@users.noreply.github.com>
2023-06-10 17:41:32 +0800

    update

--- src/components/UsedInProduction.tsx
+++ src/components/UsedInProduction.tsx
@@ -5,14 +5,14 @@ export default function UsedInProduction(): JSX.Element {
         <section className="flex flex-col items-start lg:items-center w-full">
             <h2 className="text-2xl lg:text-4xl pb-16">Powered by ZenStack</h2>
             <div className="flex flex-row items-center gap-12">
-                <a href="https://ptengine.com">
+                <a href="https://ptengine.com" target="_blank">
                     <img className="h-12" src="/img/logo/ptengine.svg" />
                 </a>
-                <a href="https://www.mermaidchart.com/">
+                <a href="https://www.mermaidchart.com/" target="_blank">
                     {' '}
                     <img className="h-8" src="/img/logo/mermaidchart.svg" />
                 </a>
-                <a href="https://forevervacation.com">
+                <a href="https://forevervacation.com" target="_blank">
                     <img className="h-12" src="/img/logo/forevervacation.png" />
                 </a>
             </div>
