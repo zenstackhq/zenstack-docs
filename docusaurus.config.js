4187e1217aab2d0d03af8b6cfb0249b861702afe
Yiming <yiming@whimslab.io>
2023-06-25 13:28:48 +0800

    docs: enable page editing (#144)
    * docs: enable page editing
    
    * update title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -34,6 +34,7 @@ const config = {
                 docs: {
                     sidebarPath: require.resolve('./sidebars.js'),
                     path: 'docs',
+                    editUrl: 'https://github.com/zenstackhq/zenstack-docs/edit/main',
                 },
                 blog: {
                     showReadingTime: true,
