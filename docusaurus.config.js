c5d6a8d4e1bf58f75048ea2a53e0e84e59d372d7
JG <jiashengguo@outlook.com>
2023-10-19 16:11:51 +0100

    add extra padding

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -117,7 +117,7 @@ const config = {
                                 <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"></path>
                                 <path d="M9 18c-4.51 2-5-2-7-2"></path>
                             </svg>
-                            <span id="github-text" class="text-white">Star on Github</span>
+                            <span id="github-text" class="text-white pr-2">Star on Github</span>
                             <span class="flex items-center ml-2 group-hover:text-yellow-500 transition-colors duration-200 ease-in-out">
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
