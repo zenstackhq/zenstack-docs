8f0a4318c1de07d9c8491ae645b145b7302a0dce
eliav2 <louski.a@gmail.com>
2024-04-10 07:54:09 +0300

    now blog posts comments apear only on each BlogPostPage and not on BlogListPage

---
+++ src/theme/BlogPostPage/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="@docusaurus/plugin-content-blog" />
+/// <reference types="react" />
+import type { Props } from '@theme/BlogPostPage';
+export default function BlogPostPage(props: Props): JSX.Element;
