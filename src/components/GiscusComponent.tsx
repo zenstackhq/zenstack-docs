e422ec3cebe4a1fd1915d5c384e60f29b20e6a1d
Eliav2 <47307889+Eliav2@users.noreply.github.com>
2024-04-11 04:37:07 +0300

    adding comments to docs pages. (#277)
    * adding comments to docs pages.
    
    * Update src/components/GiscusComponent.tsx
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * chore: update Giscus repo to zenstack-docs
    
    * fix typo
    
    * swizzeling BlogPostItem and enabling comments to blog pages as well.
    
    * now blog posts comments apear only on each BlogPostPage and not on BlogListPage
    
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    Co-authored-by: JG <jiashengguo@outlook.com>

---
+++ src/components/GiscusComponent.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import Giscus from '@giscus/react';
+import { useColorMode } from '@docusaurus/theme-common';
+
+export default function GiscusComponent() {
+    const { colorMode } = useColorMode();
+
+    return (
+        <div style={{ paddingTop: 32 }}>
+            <h6>Comments</h6>
+            <span>
+                Feel free to ask questions, give feedback, or report issues.{' '}
+                <p
+                    className={'text-sm text-gray-500 italic'}
+                    style={{
+                        display: 'inline',
+                        fontStyle: 'italic',
+                    }}
+                >
+                    Don't Spam
+                </p>
+            </span>
+            <hr />
+            <div className={'text-sm text-gray-600'}>
+                You can edit/delete your comments by going directly to the discussion, clicking on the 'comments' link
+                below
+            </div>
+            <div
+                style={{
+                    padding: 2,
+                    backgroundColor: colorMode === 'dark' ? '#1e1e1e' : 'rgb(255, 255, 255)',
+                }}
+            >
+                <Giscus
+                    repo="zenstackhq/zenstack-docs"
+                    repoId="R_kgDOIvA9Bg"
+                    category="Comments"
+                    categoryId="DIC_kwDOIvA9Bs4Cego9" // id of "Comments"
+                    mapping="pathname" // Important! To map comments to URL
+                    term="Welcome zenstack site comments!"
+                    strict="0"
+                    reactionsEnabled="1"
+                    emitMetadata="0"
+                    inputPosition="top"
+                    theme={colorMode}
+                    lang="en"
+                    loading="lazy"
+                />
+            </div>
+        </div>
+    );
+}
