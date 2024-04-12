62dfc906cba6d87af46d237a948aece3dd2d9112
Eliav2 <47307889+Eliav2@users.noreply.github.com>
2024-04-12 16:10:01 +0300

    fix Giscus color theme on initial renders (#285)
    * adding comments to docs pages.
    
    * Update src/components/GiscusComponent.tsx
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * chore: update Giscus repo to zenstack-docs
    
    * fix typo
    
    * swizzeling BlogPostItem and enabling comments to blog pages as well.
    
    * now blog posts comments apear only on each BlogPostPage and not on BlogListPage
    
    * - update dark mode color to match zenstack dark theme color
    - fix Giscus color theme on initial renders.(mounting)
    
    * fix
    
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    Co-authored-by: JG <jiashengguo@outlook.com>

--- src/components/GiscusComponent.tsx
+++ src/components/GiscusComponent.tsx
@@ -25,13 +25,9 @@ export default function GiscusComponent() {
                 You can edit/delete your comments by going directly to the discussion, clicking on the 'comments' link
                 below
             </div>
-            <div
-                style={{
-                    padding: 2,
-                    backgroundColor: colorMode === 'dark' ? '#1e1e1e' : 'rgb(255, 255, 255)',
-                }}
-            >
+            <div style={{ padding: 2 }}>
                 <Giscus
+                    key={colorMode} // This is important to force re-mounting on theme change(bug in giscus)
                     repo="zenstackhq/zenstack-docs"
                     repoId="R_kgDOIvA9Bg"
                     category="Comments"
