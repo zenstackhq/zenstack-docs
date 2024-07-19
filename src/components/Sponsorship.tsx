78f5eb49c611763fb08201eadf3ef79028392e21
JG <jiashengguo@outlook.com>
2024-07-19 20:28:36 +0800

    doc: add generous sponsor Marblism

---
+++ src/components/Sponsorship.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+export default function Sponsorship(): JSX.Element {
+    return (
+        <div className="flex justify-center w-full">
+            <div className="flex flex-col text-center xl:w-3/4">
+                <h2 className="text-2xl md:text-3xl lg:text-4xl pb-20">Our Generous Sponsors</h2>
+                <div className="flex flex-auto justify-center">
+                    {/* <a href="https://www.marblism.com/" target="_blank" className="no-underline">
+                        <img src="/img/logo/marblism-logo.png" className="h-40 object-contain" alt="marblism-logo" />
+                        <h2>Marblism</h2>
+                    </a> */}
+                    <Sponsor src="/img/logo/marblism-logo.png" name="Marblism" website="https://www.marblism.com/" />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+function Sponsor({ src, name, website }: { src: string; name: string; website: string }): JSX.Element {
+    const alt = src.split('/').pop()?.split('.')[0] ?? 'logo';
+    return (
+        <a href={website} target="_blank" className="no-underline">
+            <img src={src} className="h-40 object-contain" alt={alt} />
+            <h2>{name}</h2>
+        </a>
+    );
+}
